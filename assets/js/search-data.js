d32770ed3e6f480434459a50952c9992b90e8cef
psujunji <101603012+psujunji@users.noreply.github.com>
2025-10-30 02:17:56 +0000

    Deploying to gh-pages from @ psujunji/psujunji.github.io@1e01fde202d605e3a9f044f19da9c3c18545cc5e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,9 +16,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-research",
+          title: "Research",
+          description: "Our interest in Acoustics spans from Hz to GHz and covers both their physics and applications.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -429,51 +429,36 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_5/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-ai-hardware",
+          title: 'AI Hardware',
+          description: "Synthetic-domain computing and neural networks using lithium niobate integrated nonlinear phononics",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-cavity-electro-acoustic-dynamics",
+          title: 'Cavity electro-acoustic dynamics',
+          description: "using integrated lithium niobate phononic crystal resonators",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-low-loss-integrated-phononic-circuits",
+          title: 'Low-loss integrated phononic circuits',
+          description: "low-phase-noise acoustic oscillators",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-metamaterial-based-acoustic-devices",
+          title: 'Metamaterial-based acoustic devices',
+          description: "for audio and ultrasound applications.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-nonlinear-and-time-modulated-acoustic-metasurfaces",
+          title: 'Nonlinear and time-modulated acoustic metasurfaces',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-tbd",
+          title: 'TBD',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
