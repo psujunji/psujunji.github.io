3a3cf5cce62b7c5eb90e3f89b3c250c4dead6377
psujunji <ncsujunji@gmail.com>
2026-09-08 23:11:13 -0400

    Preserve research card layout on deploy

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,5 +2,6 @@ module.exports = {
   content: ["_site/**/*.html", "_site/**/*.js"],
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
+  safelist: ["project-card", "project-card-cover", "card-body", "card-title", "card-img-top"],
   skippedContentGlobs: ["_site/assets/**/*.html"],
 };
